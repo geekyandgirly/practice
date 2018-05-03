fa809490cc37a33aa46fba41ff66fceacdcd4879
Luping Lin <lupinglin@gmail.com>
2018-05-02 21:22:09 -0700

    improvement for IslandLandWater class.

--- src/practice/IslandLandWater.java
+++ src/practice/IslandLandWater.java
@@ -1,10 +1,12 @@
 package practice;
 
-/** https://leetcode.com/problems/max-area-of-island/ **/
+/** 
+ * https://leetcode.com/problems/max-area-of-island/
+ * 
+ **/
 
 public class IslandLandWater {
-	int len;
-
+	
     public int maxAreaOfIsland(int[][] grid) {
         int cols = grid[0].length;
         int rows = grid.length;
@@ -14,28 +16,25 @@ public class IslandLandWater {
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (grid[row][col] == 1 && !visited[row][col]) {
-                    len = 0;
-                    dfs(row, col, grid, visited, rows, cols);
-                    max = Math.max(max, len); 
+                    max = Math.max(max, dfs(row, col, grid, visited, rows, cols)); 
                 }
             }
         }
         return max;
     }
     
-    private void dfs(int row, int col, int[][] grid, boolean[][] visited, int rows, int cols) {
+    private int dfs(int row, int col, int[][] grid, boolean[][] visited, int rows, int cols) {
         if (row < 0 || col < 0 || row >= rows || col >= cols) {
-            return;
+            return 0;
         }
         if (grid[row][col] == 0 || visited[row][col]) {
-            return;
+            return 0;
         }
         visited[row][col] = true;
-        len++;
-        
-        dfs(row - 1, col, grid, visited, rows, cols); // up
-        dfs(row, col + 1, grid, visited, rows, cols); // right
-        dfs(row + 1, col, grid, visited, rows, cols); // down
-        dfs(row, col - 1, grid, visited, rows, cols); // left
+
+        return 1 + dfs(row - 1, col, grid, visited, rows, cols) // up
+        	+ dfs(row, col + 1, grid, visited, rows, cols) // right
+        	+ dfs(row + 1, col, grid, visited, rows, cols) // down
+        	+ dfs(row, col - 1, grid, visited, rows, cols); // left
     }
 }
